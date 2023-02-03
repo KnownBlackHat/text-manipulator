224b99fdba2525cca3d728431b9af973ab352084
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2023-02-03 23:45:13 +0000

    optimisation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import Navbar from "./components/Navbar.mjs"
 import Textform from "./components/Form.mjs"
 import Alert from "./components/Alert.js";
-import {useState} from "react"
+import { useState } from "react"
+
 
 function App() {
-  const [alerttext,setalert]=useState(null)
-  const showalert= (color,msg)=>{
-    setalert({color:color,msg:msg})
-    setTimeout(() => {
-      setalert(null)
-    }, 1000);
-  
+  const [alerttext, setalert] = useState([])
+  const showalert = (color, msg) => {
+    const id = Date.now()
+    setalert([...alerttext, { color: color, msg: msg, id: id }])
   }
+  // Data: [{color:"red",msg:"my-msg"},{color:"blue",msg:"new-msg"}]
   return (
     <>
       <Navbar heading="Text Manipulator" />
-      <div id="alert" className="h-[3rem] absolute right-0 top-0">
-      <Alert obj={alerttext}/>
+      <div id="alert" className="w-1/5 text-center absolute right-0 top-0">
+        {alerttext.map(element => { return <Alert key={element.id} obj={element} /> })}
+        {/* <Alert obj={alerttext}/> */}
       </div>
       <Textform alert={showalert} />
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,12 @@
-export default function Alert({obj}){
-    return(
-        obj && <div className={"rounded text-white p-4 m-4 bg-" + obj.color + "-500"}>
-            {obj.msg} 
+export default function Alert({ obj }) {
+    setTimeout(() => {
+        try {document.querySelector(`#id_${obj.id}`).remove()}
+        catch {}
+    }, 1500);
+    return (
+        <div id={`id_${obj.id}`} className={"rounded duration-500 text-white p-4 m-4 bg-" + obj.color + "-500"}>
+            {obj.msg}
+            
         </div>
     )
 }
